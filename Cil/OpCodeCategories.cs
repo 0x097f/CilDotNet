6c32205318a72293c8cd1f84576f52de0b90ebd4
ufixamp09 <1120151209@qq.com>
2026-08-28 18:41:58 +0800

    Add first work
    Uhh dunno

---
+++ Cil/OpCodeCategories.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Semicolon.Cil
+{
+    public class OpCodeCategories
+    {
+        public enum OpCode
+        {
+
+        }
+    }
+
+}
